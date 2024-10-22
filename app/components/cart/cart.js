2a4b0d657aeca30d726b2e89626faa2654e74750
stuart.roper <stuart.roper@bravissimo.com>
2024-10-22 11:14:34 +0100

    Add confirm order button

--- app/components/cart/cart.js
+++ app/components/cart/cart.js
@@ -5,6 +5,13 @@ import Image from 'next/image'
 import styles from "./cart.module.css";
 import { useDispatch, useSelector } from 'react-redux';
 import CartItem from '../cart-item/CartItem';
+import Button from '../button/button';
+
+
+let USDollar = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+});
 
 const Cart = () => {
 
@@ -14,6 +21,10 @@ const Cart = () => {
   const getTotalCartQuantity = () => {
     return cart.reduce((total, item) => total + item.qty, 0); 
   };
+
+  const getTotalCost = () => {
+    return cart.reduce((total, item) => total + (item.price * item.qty), 0)
+  }
   
 
   return (
@@ -26,8 +37,19 @@ const Cart = () => {
       {cart.map((item, key) => {
        return <CartItem item={item} />
       })}
-    </div>}
-        
+      <div className={styles.totalContainer}>
+        <div className={styles.totalDescription}>Order Total</div>
+        <div className={styles.totalAmount}>{USDollar.format(getTotalCost())}</div>
+      </div>
+    </div>
+    }
+    <div className={styles.deliveryContainer}>
+      <Image src='/assets/images/icon-carbon-neutral.svg' width={25} height={30} alt='carbon neutral logo' />
+      <p>This is a <b>carbon-neutral </b>delivery</p>
+    </div>
+    <div className={styles.buttonContainer}>
+      <button className={styles.payButton}>Confirm Order</button>
+    </div>
     </div>
   )
 }
