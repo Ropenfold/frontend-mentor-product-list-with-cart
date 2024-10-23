ef0905d0e6b1b9da70cf3f7a6bc521a69494726f
Stuart Roper <stuart.roper@bravissimo.com>
2024-10-23 10:43:55 +0100

    add modal functionality

--- app/page.js
+++ app/page.js
@@ -1,11 +1,25 @@
+'use client'
+
 import styles from "./page.module.css";
 import data from "./data/data";
 import Item from "./components/item/Item";
 import Cart from "./components/cart/cart";
+import { useSelector } from 'react-redux';
+import { Modal } from "./components/modal/Modal";
 
 export default function Home() {
+
+  const background = useSelector((state) => state.cartReducer.backgroundAndModal);
+  console.log('background', background);
+
   return (
       <div className={styles.mainContainer}>
+           {background && (
+            <>
+        <div className={styles.background} />
+          <Modal />
+          </>
+      )}
           <div className={styles.mainItemContainer}>
           <h1 className={styles.title}><b>Desserts</b></h1>
           <div className={styles.contentContainer}>
