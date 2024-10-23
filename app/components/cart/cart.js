c62df6ed9649d6f89b773272df8f5f74367ba48c
Stuart Roper <stuart.roper@bravissimo.com>
2024-10-23 10:44:48 +0100

    add cart quantity and complete order work

--- app/components/cart/cart.js
+++ app/components/cart/cart.js
@@ -5,8 +5,7 @@ import Image from 'next/image'
 import styles from "./cart.module.css";
 import { useDispatch, useSelector } from 'react-redux';
 import CartItem from '../cart-item/CartItem';
-import Button from '../button/button';
-
+import { completeOrder } from '../../../redux/actions';
 
 let USDollar = new Intl.NumberFormat('en-US', {
   style: 'currency',
@@ -14,6 +13,8 @@ let USDollar = new Intl.NumberFormat('en-US', {
 });
 
 const Cart = () => {
+
+  const dispatch = useDispatch();
 
   const cart = useSelector((state) => state.cartReducer.cart);
   console.log('cart', cart)
@@ -26,32 +27,43 @@ const Cart = () => {
     return cart.reduce((total, item) => total + (item.price * item.qty), 0)
   }
   
+  const confirmOrder = () => {
+    dispatch(completeOrder())
+  }
 
   return (
-    <div className={styles.cartContainer} >
-        <h2 className={styles.cartText}><b>{`Your Cart (${getTotalCartQuantity()})`}</b></h2>
-        {cart.length === 0 ? <div className={styles.itemList}>
-    <Image src='/assets/images/illustration-empty-cart.svg' width={128} height={128} alt='empty cart'/>
-    <p className={styles.emptyCartText}>Your added items will appear here</p>
-    </div> : <div className={styles.itemList}>
-      {cart.map((item, key) => {
-       return <CartItem item={item} />
-      })}
-      <div className={styles.totalContainer}>
-        <div className={styles.totalDescription}>Order Total</div>
-        <div className={styles.totalAmount}>{USDollar.format(getTotalCost())}</div>
-      </div>
+    <div className={styles.cartContainer}>
+      <h2 className={styles.cartText}>
+        <b>{`Your Cart (${getTotalCartQuantity()})`}</b>
+      </h2>
+      {cart.length === 0 ? (
+        <div className={styles.itemList}>
+          <Image src='/assets/images/illustration-empty-cart.svg' width={128} height={128} alt='empty cart' />
+          <p className={styles.emptyCartText}>Your added items will appear here</p>
+        </div>
+      ) : (
+        <>
+          <div className={styles.itemList}>
+            {cart.map((item, key) => {
+              return <CartItem key={key} item={item} />;
+            })}
+            <div className={styles.totalContainer}>
+              <div className={styles.totalDescription}>Order Total</div>
+              <div className={styles.totalAmount}>{USDollar.format(getTotalCost())}</div>
+            </div>
+          </div>
+          <div className={styles.deliveryContainer}>
+            <Image src='/assets/images/icon-carbon-neutral.svg' width={25} height={30} alt='carbon neutral logo' />
+            <p>This is a <b>carbon-neutral</b> delivery</p>
+          </div>
+  
+          <div className={styles.buttonContainer}>
+            <button className={styles.payButton} onClick={() => confirmOrder()}>Confirm Order</button>
+          </div>
+        </>
+      )}
     </div>
-    }
-    <div className={styles.deliveryContainer}>
-      <Image src='/assets/images/icon-carbon-neutral.svg' width={25} height={30} alt='carbon neutral logo' />
-      <p>This is a <b>carbon-neutral </b>delivery</p>
-    </div>
-    <div className={styles.buttonContainer}>
-      <button className={styles.payButton}>Confirm Order</button>
-    </div>
-    </div>
-  )
+  );  
 }
 
 export default Cart
